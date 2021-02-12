fc9de9f7e05d0de4de3fc90d66c4f9def6e47a9f
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-02-12 14:25:20 +0100

    webgl renderer

--- public/WebGLRenderer.js
+++ public/WebGLRenderer.js
@@ -1,36 +1,7 @@
-export default class WebGLRenderer {
+import Renderer from "./Renderer.js";
 
-    constructor() {
-        this.canvas = document.createElement('canvas');
-        this.context = this.canvas.getContext("2d");
+export default class WebGLRenderer extends Renderer {
 
-        document.body.appendChild(this.canvas);
-
-        window.addEventListener('resize', e => this.format());
-
-        this.format();
-    }
-
-    format() {
-        this.canvas.width = this.canvas.parentNode.clientWidth;
-        this.canvas.height = this.canvas.parentNode.clientHeight;
-    }
-
-    draw(camera) {
-        const ctxt = this.context;
-        ctxt.restore();
-        ctxt.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        ctxt.save();
-
-        ctxt.scale(1, -1);
-        ctxt.translate(0, -ctxt.canvas.height);
-        ctxt.translate(
-            -camera.position.x,
-            -camera.position.y
-        );
-
-        camera.width = this.canvas.width;
-        camera.height = this.canvas.height;
-    }
+    
 
 }

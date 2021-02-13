9418a66972191f5cc4007f9be3e2dc1cf8a9868e
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-02-13 11:35:18 +0100

    join btn

--- public/utils.js
+++ public/utils.js
@@ -6,3 +6,26 @@ function uuidv4() {
 		return v.toString(16);
 	});
 }
+
+window.onload = () => {
+	navigator.registerProtocolHandler("web+uncut", "http://127.0.0.1:5500/public/index.html?connect=%s", "Uncut2 Multiplayer");
+
+	const a = document.createElement('a');
+	a.href = "web+uncut:" + client.id;
+	a.className = "join-btn";
+	a.innerHTML = "Join Game";
+	document.body.appendChild(a);
+
+	const search = parseSearch(location.search);
+	const connId = search.connect;
+
+	console.log(connId);
+}
+
+function parseSearch(str) {
+    const res = {};
+    str.substring(1).split("&").map(item => item.split("=")).forEach(item => {
+        res[item[0]] = unescape(item[1]);
+    });
+    return res;
+}
